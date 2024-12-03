357d829092bc27528068f0da6caab89be6269ace
m-imtiaj <Mohammad.imtiaj58@gmail.com>
2024-12-03 12:40:21 +0000

    Final Version

---
+++ v2/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'restaurant';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Database connection failed: " . $e->getMessage();
+}
+?>
+
